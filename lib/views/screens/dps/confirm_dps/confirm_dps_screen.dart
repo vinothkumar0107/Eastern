5337fb28f4e387616875cd5df696effba2a00b71
senioriosdev123 <Atyourtime123>
2024-10-04 21:12:13 +0530

    DPS and withdraw confirm screen design change.

--- lib/views/screens/dps/confirm_dps/confirm_dps_screen.dart
+++ lib/views/screens/dps/confirm_dps/confirm_dps_screen.dart
@@ -14,6 +14,10 @@ import 'package:eastern_trust/views/components/buttons/rounded_loading_button.da
 import 'package:eastern_trust/views/components/custom_loader.dart';
 import 'package:eastern_trust/views/screens/fdr/confirm_fdr/widget/preview_row.dart';
 
+import '../../../../core/route/route.dart';
+import '../../../components/appbar/appbar_specific_device.dart';
+import '../../../components/will_pop_widget.dart';
+
 class ConfirmDPSScreen extends StatefulWidget {
   const ConfirmDPSScreen({Key? key}) : super(key: key);
 
@@ -37,10 +41,10 @@ class _ConfirmDPSScreenState extends State<ConfirmDPSScreen> {
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build2(BuildContext context) {
     return GetBuilder<DPSConfirmController>(builder: (controller)=>Scaffold(
       backgroundColor: MyColor.getScreenBgColor(),
-      appBar: const CustomAppBar(title: MyStrings.dpsApplicationPreview),
+      appBar: const CustomAppBar(title: MyStrings.dpsApplicationPreview, isTitleCenter: false,),
       body: controller.isLoading?const CustomLoader():Center(
         child: SingleChildScrollView(
           child: Container(
@@ -93,5 +97,126 @@ class _ConfirmDPSScreenState extends State<ConfirmDPSScreen> {
         ),
       ),
     ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<DPSConfirmController>(
+      builder: (controller) => WillPopWidget(
+        nextRoute: '',
+        child: GestureDetector(
+          onTap: () {
+            FocusScopeNode currentFocus = FocusScope.of(context);
+            if (!currentFocus.hasPrimaryFocus) {
+              currentFocus.unfocus();
+            }
+          },
+          child: Scaffold(
+            appBar: AppBarSpecificScreen.buildAppBar(),
+            backgroundColor: MyColor.appPrimaryColorSecondary2,
+            body: Stack(
+              children: [
+                const GradientView(gradientViewHeight: 3.5),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+                  decoration: const BoxDecoration(
+                    color: Colors.transparent,
+                  ),
+                  child: Row(
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.arrow_back, color: Colors.white),
+                        onPressed: () {
+                          Get.back();
+                        },
+                      ),
+                      Expanded(
+                        child: Text(
+                            MyStrings.dpsApplicationPreview,
+                            textAlign: TextAlign.left,
+                            style: interSemiBoldOverLarge.copyWith(color: MyColor.colorWhite,decorationColor:MyColor.primaryColor)
+                        ),
+                      ),
+                      // To keep the title centered, you can add an empty `SizedBox`
+                      const SizedBox(width: 48.0),
+                    ],
+                  ),
+                ),
+                controller.isLoading ?
+                const Center(
+                    child: CircularProgressIndicator(color: MyColor.primaryColor)
+                ) :
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Container(
+                    padding: const EdgeInsets.fromLTRB(15.0, 70.0, 15.0, 0.0),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          Card(
+                            color: MyColor.colorWhite,
+                            elevation: 1,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                            child: Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: controller.isLoading?const CustomLoader():Center(
+                                  child: SingleChildScrollView(
+                                    child:  Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: [
+                                        Text(MyStrings.youHaveRequestToDPSInvestMsg.tr,style: interSemiBold.copyWith(fontSize: Dimensions.fontLarge)),
+                                        Text('(${MyStrings.beSureBeforeConfirm.tr})',style: interRegularDefault.copyWith(color: MyColor.colorRed),),
+                                        const SizedBox(height: 25,),
+                                        PreviewRow(firstText: MyStrings.plan, secondText: controller.plan?.name??''),
+                                        PreviewRow(firstText: MyStrings.installmentInterval, secondText: '${controller.plan?.installmentInterval??''} ${MyStrings.days}'),
+                                        PreviewRow(firstText: MyStrings.totalInstallment, secondText: controller.plan?.totalInstallment??''),
+                                        PreviewRow(firstText: MyStrings.perInstallment, secondText: '${controller.currencySymbol}${Converter.formatNumber(controller.plan?.perInstallment??'')}'),
+                                        PreviewRow(firstText: MyStrings.totalDeposit, secondText: '${controller.currencySymbol}${controller.getDepositAmount()}'),
+                                        PreviewRow(firstText: MyStrings.profitRate, secondText: '${Converter.roundDoubleAndRemoveTrailingZero(controller.plan?.interestRate??'0')}%'),
+                                        PreviewRow(firstText: MyStrings.withdrawableAmount, secondText: '${controller.currencySymbol}${Converter.formatNumber(controller.plan?.finalAmount??'')}',showDivider: false),
+                                        const SizedBox(height: 15,),
+                                        Text('* ${MyStrings.ifAnInstallmentIsDelayedFor.tr} ${controller.plan?.delayValue??'1'} ${MyStrings.orMoreDaysThen.tr} ${controller.currencySymbol}${controller.delayCharge} ${MyStrings.willBeAppliedForEachDay.tr}'.tr,style: interRegularDefault.copyWith(fontSize:Dimensions.fontSmall,color: MyColor.redCancelTextColor),),
+                                        const SizedBox(height: 8),
+                                        Text('* ${MyStrings.totalChargeAmountMsg.tr}',style: interRegularDefault.copyWith(color: MyColor.redCancelTextColor,fontSize: Dimensions.fontSmall),),
+                                        const SizedBox(height: 35,),
+                                        Row(
+                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Expanded(child: RoundedButton(color:MyColor.colorBlack,text: MyStrings.cancel.tr, press:(){
+                                              controller.cancelDPSRequest();
+                                            })),
+                                            const SizedBox(width: 25),
+                                            Expanded(child: controller.isSubmitLoading?
+                                            const RoundedLoadingBtn():
+                                            RoundedButton(
+                                                color:MyColor.primaryColor,
+                                                text: MyStrings.confirm.tr,
+                                                press:(){
+                                                  controller.confirmDPSRequest();
+                                                }))
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                            ),
+                          ),
+                          const SizedBox(height: Dimensions.space20), // Add spacing if needed
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/views/screens/withdraw/confirm_withdraw_screen/withdraw_confirm_screen.dart
+++ lib/views/screens/withdraw/confirm_withdraw_screen/withdraw_confirm_screen.dart
@@ -44,126 +44,125 @@ class _WithdrawConfirmScreenState extends State<WithdrawConfirmScreen> {
   @override
   Widget build(BuildContext context) {
 
-    return GetBuilder<WithdrawConfirmController>(builder: (controller)=>SafeArea(
-      child: Scaffold(
-          backgroundColor: MyColor.colorWhite,
-          appBar: const CustomAppBar(title: MyStrings.withdrawConfirm),
-          body: controller.isLoading?const CustomLoader():SingleChildScrollView(
-            padding: Dimensions.previewPaddingHV,
-            child: Container(
-              alignment: Alignment.center,
-              padding: const EdgeInsets.symmetric(horizontal: 12,vertical: 25),
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  color: MyColor.colorWhite,
-                  borderRadius: BorderRadius.circular(Dimensions.defaultRadius),
-                  border: Border.all(color: MyColor.borderColor,width: 1)
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-
-                  ListView.builder(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      scrollDirection: Axis.vertical,
-                      itemCount: controller.formList.length,
-                      itemBuilder: (ctx,index){
-                        FormModel? model=controller.formList[index];
-                        return  Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              model.type=='text'?Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  CustomTextField(
-                                      hintText: '${((model.name??'').capitalizeFirst)?.tr}',
-                                      needLabel: true,
-                                      needOutlineBorder: true,
-                                      labelText:( model.name??'').tr,
-                                      isRequired: model.isRequired=='optional'?false:true,
-                                      onChanged: (value){
-                                        controller.changeSelectedValue(value, index);
-                                      }),
-                                  const SizedBox(height: 10,),
-                                ],
-                              ):model.type=='textarea'?Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  CustomTextField(
-                                      needLabel: true,
-                                      needOutlineBorder: true,
-                                      labelText: (model.name??'').tr,
-                                      isRequired: model.isRequired=='optional'?false:true,
-                                      hintText: '${((model.name??'').capitalizeFirst)?.tr}',
-                                      onChanged: (value){
-                                        controller.changeSelectedValue(value, index);
-                                      }),
-                                  const SizedBox(height: 10,),
-                                ],
-                              ):model.type=='select'?Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  FormRow(label: model.name??'', isRequired: model.isRequired=='optional'?false:true),
-                                  CustomDropDownTextField(list: model.options??[],onChanged: (value){
-                                    controller.changeSelectedValue(value,index);
-                                  },selectedValue: model.selectedValue,),
-                                ],
-                              ):model.type=='radio'?Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  FormRow(label: model.name??'', isRequired: model.isRequired=='optional'?false:true),
-                                  CustomRadioButton(title:model.name,selectedIndex:controller.formList[index].options?.indexOf(model.selectedValue??'')??0,list: model.options??[],onChanged: (selectedIndex){
-                                    controller.changeSelectedRadioBtnValue(index,selectedIndex);
-                                  },),
-                                ],
-                              ):model.type=='checkbox'?Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  FormRow(label: model.name??'', isRequired: model.isRequired=='optional'?false:true),
-                                  CustomCheckBox(selectedValue:controller.formList[index].cbSelected,list: model.options??[],onChanged: (value){
-                                    controller.changeSelectedCheckBoxValue(index,value);
-                                  },),
-                                ],
-                              ):model.type=='file'?Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  FormRow(label: model.name??'', isRequired: model.isRequired=='optional'?false:true),
-                                  Padding(
-                                      padding: const EdgeInsets.symmetric(vertical: 15,horizontal: 10),
-                                      child: SizedBox(
-                                        child:InkWell(
-                                            onTap: (){
-                                              controller.pickFile(index);
-                                            }, child: ChooseFileItem(fileName: model.selectedValue??MyStrings.chooseFile.tr,)),
-                                      )
-                                  )
-                                ],
-                              ):const SizedBox(),
-
-                              const SizedBox(height: 5,),
-
-                            ],
-                          );
-                      }
-                  ),
-                  const SizedBox(height: Dimensions.space25),
-                  controller.submitLoading ?
-                  const Center(child:RoundedLoadingBtn()) :
-                  RoundedButton(
-                    color: MyColor.getButtonColor(),
-                    press: () {
-                      controller.submitConfirmWithdrawRequest();
-                    },
-                    text: MyStrings.submit.tr,
-                    textColor: MyColor.getButtonTextColor(),
-                  ),
-                ],
-              ),
+    return GetBuilder<WithdrawConfirmController>(builder: (controller)=>Scaffold(
+        backgroundColor: MyColor.colorWhite,
+        appBar: const CustomAppBar(title: MyStrings.withdrawConfirm),
+        body: controller.isLoading?const CustomLoader():SingleChildScrollView(
+          padding: Dimensions.previewPaddingHV,
+          child: Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.symmetric(horizontal: 12,vertical: 25),
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                color: MyColor.colorWhite,
+                borderRadius: BorderRadius.circular(Dimensions.defaultRadius),
+                border: Border.all(color: MyColor.borderColor,width: 1)
             ),
-          )
-      ),
-    ));
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+
+                ListView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    scrollDirection: Axis.vertical,
+                    itemCount: controller.formList.length,
+                    itemBuilder: (ctx,index){
+                      FormModel? model=controller.formList[index];
+                      return  Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          model.type=='text'?Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              CustomTextField(
+                                  hintText: '${((model.name??'').capitalizeFirst)?.tr}',
+                                  needLabel: true,
+                                  needOutlineBorder: true,
+                                  labelText:( model.name??'').tr,
+                                  isRequired: model.isRequired=='optional'?false:true,
+                                  onChanged: (value){
+                                    controller.changeSelectedValue(value, index);
+                                  }),
+                              const SizedBox(height: 10,),
+                            ],
+                          ):model.type=='textarea'?Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              CustomTextField(
+                                  needLabel: true,
+                                  needOutlineBorder: true,
+                                  labelText: (model.name??'').tr,
+                                  isRequired: model.isRequired=='optional'?false:true,
+                                  hintText: '${((model.name??'').capitalizeFirst)?.tr}',
+                                  onChanged: (value){
+                                    controller.changeSelectedValue(value, index);
+                                  }),
+                              const SizedBox(height: 10,),
+                            ],
+                          ):model.type=='select'?Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              FormRow(label: model.name??'', isRequired: model.isRequired=='optional'?false:true),
+                              CustomDropDownTextField(list: model.options??[],onChanged: (value){
+                                controller.changeSelectedValue(value,index);
+                              },selectedValue: model.selectedValue,),
+                            ],
+                          ):model.type=='radio'?Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              FormRow(label: model.name??'', isRequired: model.isRequired=='optional'?false:true),
+                              CustomRadioButton(title:model.name,selectedIndex:controller.formList[index].options?.indexOf(model.selectedValue??'')??0,list: model.options??[],onChanged: (selectedIndex){
+                                controller.changeSelectedRadioBtnValue(index,selectedIndex);
+                              },),
+                            ],
+                          ):model.type=='checkbox'?Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              FormRow(label: model.name??'', isRequired: model.isRequired=='optional'?false:true),
+                              CustomCheckBox(selectedValue:controller.formList[index].cbSelected,list: model.options??[],onChanged: (value){
+                                controller.changeSelectedCheckBoxValue(index,value);
+                              },),
+                            ],
+                          ):model.type=='file'?Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              FormRow(label: model.name??'', isRequired: model.isRequired=='optional'?false:true),
+                              Padding(
+                                  padding: const EdgeInsets.symmetric(vertical: 15,horizontal: 10),
+                                  child: SizedBox(
+                                    child:InkWell(
+                                        onTap: (){
+                                          controller.pickFile(index);
+                                        }, child: ChooseFileItem(fileName: model.selectedValue??MyStrings.chooseFile.tr,)),
+                                  )
+                              )
+                            ],
+                          ):const SizedBox(),
+
+                          const SizedBox(height: 5,),
+
+                        ],
+                      );
+                    }
+                ),
+                const SizedBox(height: Dimensions.space25),
+                controller.submitLoading ?
+                const Center(child:RoundedLoadingBtn()) :
+                RoundedButton(
+                  color: MyColor.getButtonColor(),
+                  press: () {
+                    controller.submitConfirmWithdrawRequest();
+                  },
+                  text: MyStrings.submit.tr,
+                  textColor: MyColor.getButtonTextColor(),
+                ),
+              ],
+            ),
+          ),
+        )
+    )
+    );
   }
 }
